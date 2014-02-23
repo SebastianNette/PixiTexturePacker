236f0dcd83f7f8eab61df9628c0beace33accad4
SebastianNette <sebastian-nette@gmx.de>
2014-02-23 02:12:31 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,20 +3,19 @@ if(!window.console) { window.console = { log: function(){} }; }
 
 /* require.js config */
 require.config({
-  baseUrl				: 'js/',
-  urlArgs				: 'v=' + new Date().getTime(),// <- dev 'v=0.0.1',
-  waitSeconds			: 15,
+  baseUrl: 'js/',
+  waitSeconds: 15,
   paths: {
-    'class'				: 'lib/class',
-    'jquery'			: 'lib/jquery',
-    'jquery-ui'			: 'lib/jquery-ui',
-    'underscore'		: 'lib/underscore',
-    'pixi'				: 'lib/pixi',
-    'text'				: 'lib/text'
+    'class': 'lib/class',
+    'jquery': 'lib/jquery',
+    'jquery-ui': 'lib/jquery-ui',
+    'underscore': 'lib/underscore',
+    'pixi': 'lib/pixi',
+    'text': 'lib/text'
   },
   shim: {
-    'util'				: [ 'jquery' ],
-    'jquery-ui'			: [ 'jquery' ]
+    'util': [ 'jquery' ],
+    'jquery-ui': [ 'jquery' ]
   }
 });
 
